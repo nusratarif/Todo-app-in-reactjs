a2a6be9ff241d019b22b3528ca9f08fa3a38d1ea
nusratarif <nusratarif02@gmail.com>
2020-11-11 09:17:06 +0500

    added build

---
+++ build/precache-manifest.c399381054bb6953e711c46347b760e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07960e1fa65539815f3f8cfff511350a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ad7b77de19b1f58a88",
+    "url": "/static/css/main.deb44e4e.chunk.css"
+  },
+  {
+    "revision": "3f1f01a689f2b32ed132",
+    "url": "/static/js/2.a364d41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a364d41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ad7b77de19b1f58a88",
+    "url": "/static/js/main.58dafae9.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
